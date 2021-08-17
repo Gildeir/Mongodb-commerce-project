fd9674147f26477c7bdad4ccaa14ff570c7ce92b
Gildeir <gildeirlopes@gmail.com>
2021-08-17 19:50:19 -0300

    adicionando requisito 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -18,3 +18,24 @@ db.produtos.find({}, {
   nome: 1,
   tags: 1,
 });
+
+db.produtos.updateMany({
+  valoresNutricionais: {
+     $elemMatch: {
+      tipo: "sódio",
+      percentual: {
+        $gt: 20,
+      },
+    },
+  },
+}, {
+  $push: {
+    tags: "contém sódio",
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  tags: 1,
+});
---
+++ challenges/desafio14.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      percentual: {
+        $gt: 20, $lt: 40,
+      },
+    },
+  },
+}, {
+  $push: {
+    tags: "contém sódio",
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  tags: 1,
+});
