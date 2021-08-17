39b4007b676779eb3698299ebf1edec085e9a1b5
Gildeir <gildeirlopes@gmail.com>
2021-08-17 18:09:32 -0300

    adicionando requisito 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany({}, {
+  $push: {
+    valoresNutricionais: {
+      $each: [],
+      $sort: {
+        percentual: -1,
+      },
+    },
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  valoresNutricionais: 1,
+});
