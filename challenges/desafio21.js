42117445a724f0beb5fea13edff7d0ebbe3754b1
Gildeir <gildeirlopes@gmail.com>
2021-08-17 23:06:10 -0300

    adicionando requisito 22

---
+++ challenges/desafio21.js
@@ -0,0 +1,9 @@
+db.produtos.find({
+  $expr: {
+    $gt: ["$curtidas", "$vendidos"],
+  },
+
+}, {
+  _id: 0,
+  nome: 1,
+});
---
+++ challenges/desafio22.js
@@ -0,0 +1,9 @@
+db.produtos.find({
+  vendidos: {
+    $mod: [5, 0],
+  },
+}, {
+  _id: 0,
+  nome: 1,
+  vendidos: 1,
+});
